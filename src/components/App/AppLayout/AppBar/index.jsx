b6b72296a12e75be85c192ffa52caacf148d7f12
Veselin <veselin.lk.petrov@gmail.com>
2019-02-21 15:32:16 +0200

    styled app bar

--- src/components/App/AppLayout/AppBar/index.jsx
+++ src/components/App/AppLayout/AppBar/index.jsx
@@ -4,16 +4,38 @@ import styled from 'styled-components'
 
 const Bar = styled.div`
     display: grid;
+    margin-bottom: 40px;
     grid-template-columns: 180px auto 100px 100px;
 `
+
+const ControlButtonElem = styled.div`
+    text-shadow: ${({ active }) => active && '0 0 60px #03ff03'};
+    cursor: pointer;
+`
+
+const Logo = styled.div`
+    font-size: 1.5em;
+`
+
+function toProperCase(str) {
+    return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase()
+}
+
+function ControlButton({ active, name }) {
+    return (
+        <ControlButtonElem active={active}>
+            {toProperCase(name)}
+        </ControlButtonElem>
+    )
+}
 
 export default function () {
     return (
         <Bar>
-            <div>CryptoDash</div>
+            <Logo>CryptoDash</Logo>
             <div />
-            <div>Dashboard</div>
-            <div>Settings</div>
+            <ControlButton active name="dashboard" />
+            <ControlButton name="settings" />
         </Bar>
     )
 }
