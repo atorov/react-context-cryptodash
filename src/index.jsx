2b79484577f9a0cf2dbffd7eb3733eac3135c87a
Veselin <veselin.lk.petrov@gmail.com>
2019-03-03 20:36:07 +0200

    app prepared for deployment

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import * as serviceWorker from './serviceWorker'
-
 import App from './components/App'
 
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-serviceWorker.unregister()
